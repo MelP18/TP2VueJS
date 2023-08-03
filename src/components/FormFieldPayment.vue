f21f0d4fbcb24263728b2d4d1728da74a13722cf
MelP <priscagnansounou99@gmail.com>
2023-08-03 19:04:17 +0100

    add all files

--- src/components/FormFieldPayment.vue
+++ src/components/FormFieldPayment.vue
@@ -21,3 +21,60 @@
         </div> 
     </div>
 </template>
+
+<style scoped>
+.form__content__bottom__field {
+    display: flex;
+    flex-direction: column;
+    padding: 15px;
+    gap: 20px;
+    background-color: white;
+    /* background-color: rgb(248, 154, 154); */
+}
+.form__field__name {
+    display: flex;
+    flex-direction: column;
+    gap: 7px;
+}
+.form__field__name input {
+    width: 100%;
+    padding: 8px 0 8px 5px;
+    outline: 0;
+}
+.form__field__tel {
+    display: flex;
+    flex-direction: column;
+    gap: 7px;
+}
+.select__country {
+    display: flex;
+}
+.select__country input {
+    width: 100%;
+    padding: 8px 0 8px 5px;
+    outline: 0;
+}
+.select__country select {
+    border-right: none;
+    padding: 8px 0 8px 5px;
+    outline: 0;
+    background-color: transparent;
+}
+.form__field__amount__due {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(205, 22, 22);
+    padding: 10px 0;
+    gap: 8px;
+}
+.form__field__amount__due strong {
+    color: white;
+    font-size: 18px;
+}
+.form__field__amount__due p {
+    color: white;
+    font-size: 20px;
+    font-weight: bold;
+}
+</style>
